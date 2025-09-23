f5ec696a8cddd5180f634b50df865154d12d0967
artevely <artemyveliky02@gmail.com>
2025-09-23 16:29:46 +0400

    Add workflow for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'https://kskanaeva.ru/',
+  base: '/kskanaeva/',
   plugins: [react()]
 })
